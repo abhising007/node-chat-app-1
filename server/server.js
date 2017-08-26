8c9ae458bfcaa0088eb3e6dcdabedd02086b4fbd
Abhishek Singhal <absing@sapient.com>
2017-08-26 09:50:49 +0530

    initial project setup

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+app.use(express.static(publicPath));
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log(`Server is up at ${port}`);
+});
+
+module.exports = {
+    app
+};
