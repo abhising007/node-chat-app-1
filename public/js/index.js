af65345f9d1b2eb59bea52bd7f383e7096ba600a
Abhishek Singhal <absing@sapient.com>
2017-08-26 10:55:14 +0530

    added newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket = io();
+socket.on('connect', function() {
+    console.log('connected to server');
+
+    // emit createMessage event to the server
+    socket.emit('createMessage',{
+        from: 'abs2',
+        text: 'message for abs2'
+    });
+    
+});
+
+socket.on('disconnect', function() {
+    console.log('disconnected from server');
+});
+
+// listen to newMessage event from server
+socket.on('newMessage', function(message){
+    console.log('New message: ', message);
+});
--- server/server.js
+++ server/server.js
@@ -18,9 +18,22 @@ var port = process.env.PORT || 3000;
 io.on('connection', (socket)=>{
     console.log('New user connected');
 
+    // create newMessage custom event to be emitted to clients
+    socket.emit('newMessage', {
+        to: 'abs1',
+        text: 'message for abs1',
+        createdAt: 123
+    });
+    
+    // event coming from client
+    socket.on('createMessage', (newMessage)=> {
+        console.log('create message with: ', newMessage);
+    });
+
     socket.on('disconnect', () => {
         console.log('user was disconnected');
     });
+
 });
 
 server.listen(port, ()=>{
