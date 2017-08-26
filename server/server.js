f3825ac5b19ffbf6058c9b51374bacabe120ea7a
Abhishek Singhal <absing@sapient.com>
2017-08-26 10:26:29 +0530

    added socket.io with connect and disconnect events

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 const path = require('path');
+const http = require('http');
+
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app); // configure the server
+var io = socketIO(server); // create webSockets server
+
 app.use(express.static(publicPath));
 
 var port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
+// socket variable in index.html for websocket connection
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user was disconnected');
+    });
+});
+
+server.listen(port, ()=>{
     console.log(`Server is up at ${port}`);
 });
 
